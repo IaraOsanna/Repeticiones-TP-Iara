498457f2b7c960ee3df799ca62ecf59b79f81ab2
IaraOsanna <166017131+IaraOsanna@users.noreply.github.com>
2024-06-27 18:43:48 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+//1) 
+let indice = 1
+for ( indice = 1; indice < 11; indice = indice + 1) {
+    console.log(indice)
+    
+}
+
+//2) 
+let indice2 = 5
+for ( indice = 15; indice > 5; indice = indice - 1) {
+    console.log(indice2)
+    
+}
+
+//3)
+let indice3 = 1
+for ( indice3 = 1; indice < 100; indice = indice + 2){
+    console.log(indice3)
+    
+}
+
+//4)
+let indice4 = 0
+for  ( indice4 = 0; indice < 70; indice = indice + 7){
+    console.log(index4)
+    
+}
